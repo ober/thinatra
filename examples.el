89f72bf3449fb1edc3cc4c8a68c0f9c7587d0da9
Jaime Fournier <jaimef@linbsd.org>
2013-04-20 19:17:30 -0700

    updates for go-lang

--- examples.el
+++ examples.el
@@ -29,7 +29,7 @@
 ;; generate random secure password
 ;;(require 'supergenpass) ;; M-x package-install <ret> supergenpass <ret>
 ;; (get "/pwgen"
-;;   (s-pwgen))
+;;   (s-pwgen)) # !> useless use of - in void context
 
 ;; Show all of the vars passed in.
 (get "/test1/:a/:b/:c/:d"
@@ -38,6 +38,60 @@
 ;; Route patterns may also include splat (or wildcar) parameters
 (get "/say/*/to/*"
   splat)
+
+;; Evaluate Go-lang Fibonacci
+(require 'das-ich)
+(get-go "/fib/:number"
+  "
+  ;; (setq code
+;;    (ris "
+;; package main
+
+;; import (
+;; 	\"fmt\"
+;; 	\"flag\"
+;; )
+
+;; func fib(n int) uint64 {
+;; 	if n < 2 {
+;; 		return 1
+;; 	}
+;; 	return (fib(n-1) + fib(n-2))
+;; }
+
+;; func fib2(n int) uint64 {
+;; 	if n < 2 {
+;; 		return 1
+;; 	}
+;; 	var n1 uint64 = 1
+;; 	var n2 uint64 = 1
+;; 	for i := 2; i < n; i++ {
+;; 		t := n2
+;; 		n2 += n1
+;; 		n1 = t
+;; 	}
+;; 	return n2
+;; }
+
+;; func main() {
+;; 	var n int
+;; 	var f bool
+;; 	flag.IntVar(&n, \"n\", #{number}, \"number to calc\")
+;; 	flag.BoolVar(&f, \"f\", true, \"func to use(default=fast)\")
+;; 	flag.Parse()
+;; 	var x uint64 = 0
+;; 	if f {
+;; 		x = fib2(n)
+;; 	} else {
+;; 		x = fib(n)
+;; 	}
+;; 	fmt.Printf(\"fib[%d]=%d\n\", n, x)
+;; }
+;; "
+;; "1"
+  )
+
+
 
 ;; Add two numbers
 (get "/sum/:a/:b"
